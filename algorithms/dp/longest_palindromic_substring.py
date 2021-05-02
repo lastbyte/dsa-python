6929ba255a25f991a4d8284848e1a864c2b07d53
nishant <nishant.9c9@gmail.com>
2021-05-03 00:52:55 +0530

    refractored function

--- algorithms/dp/longest_palindromic_substring.py
+++ algorithms/dp/longest_palindromic_substring.py
@@ -18,13 +18,7 @@ class Solution:
             max_len = 1
             palindrome = s[i:i + 1]
 
-        for i in range(length-1):
-            if s[i] == s[i+1]:
-                memory[2][i] = True
-                max_len = 2
-                palindrome = s[i:i+2]
-
-        for i in range(3, length + 1):
+        for i in range(2, length + 1):
             for j in range(length + 1 - i):
                 memory[i][j] = True if memory[i-2][j+1] is True and s[j] == s[j+i-1] else False
                 if memory[i][j] is True and i > max_len:
