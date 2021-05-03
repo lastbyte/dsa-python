cf093ce1cfb5a7fa5bd02f901fa9e155361eb440
nishant <nishant.9c9@gmail.com>
2021-05-04 02:00:50 +0530

    removed duplicates

--- general/reverse_integer.py
+++
@@ -1,7 +0,0 @@
-def reverse_integer(num : int) -> int :
-    if num == 0 or int ( str(abs(num))[::-1]) > pow(2, 31) :
-        return 0;
-    return ( num // abs(num) ) * int ( str(abs(num))[::-1])
-
-print(reverse_integer(1563847412))
-print(reverse_integer(-120))
