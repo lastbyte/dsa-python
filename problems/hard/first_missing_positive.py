66caea84cc9ecb68c827c139ef3cb2c1cf350c78
nishant <nishant.9c9@gmail.com>
2021-05-05 23:51:57 +0530

    added optimised solution for first missing positive integer

--- problems/hard/first_missing_positive.py
+++ problems/hard/first_missing_positive.py
@@ -33,22 +33,37 @@ link -> https://leetcode.com/problems/first-missing-positive/
 def first_missing_positive(nums):
     smallest_positive = 1
     nums.sort()
-    for i in range(len(nums)):
+    for i in range(size):
         if nums[i] == smallest_positive:
             smallest_positive += 1
     return smallest_positive
 
 
 def first_missing_positive_1(nums):
-    smallest_positive = 1
-    nums.sort()
-    for i in range(len(nums)):
-        if nums[i] == smallest_positive:
-            smallest_positive += 1
-    return smallest_positive
+    size = len(nums)
+    if 1 not in nums:
+        return 1   
+    
+    if size == 1:
+        return 2
+     
+    for i in range(size):
+        if nums[i] <= 0 or nums[i] > size: nums[i] = 1
+
+    for i in range(size):
+        index = abs(nums[i])
+        if index == size :
+            nums[0] = - abs(nums[0])
+        else:
+            nums[index] = - abs(nums[index])
+    
+    for i in range(1,size):
+        if nums[i] > 0 : return i
+    
+    return size if nums[0] > 0 else size+1
 
 
 if __name__ == "__main__":
-    nums = [2, 7, 6, 5, 4, 3, 8, 1]
-    result = first_missing_positive(nums)
+    nums = [2, 2, 6, 5, 4, 3, 8, 1]
+    result = first_missing_positive_1(nums)
     print(result)
