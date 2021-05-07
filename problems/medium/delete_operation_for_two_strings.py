bca40ae5d025365580f7dfb3f7335a902c2bf634
nishant <nishant.9c9@gmail.com>
2021-05-07 23:08:39 +0530

    delete distance between two strings

--- problems/medium/delete_operation_for_two_strings.py
+++ problems/medium/delete_operation_for_two_strings.py
@@ -24,11 +24,33 @@ word1 and word2 consist of only lowercase English letters.
 link -> https://leetcode.com/problems/delete-operation-for-two-strings/
 
 '''
+
+
+
+
 class Solution:
-    def min_distance(self, word1,word2):
-        memory = [[float('inf')] * len(word1) for i in range(len(word2))]
-        print(memory)
-        pass
+
+    def min_distance(self, word1, word2):
+        distance_memory = [[float('inf')] * (len(word2) + 1 ) for i in range(len(word1)+1)]
+        return self.min_distance_util(word1, word2, distance_memory);
+
+
+    def min_distance_util(self, word1,word2, distance_memory):
+        
+        #initailize memory
+
+        distance_memory[0][0] = 0
+        for w1 in range(1, len(word1) + 1) : distance_memory[w1][0] = w1
+        for w2 in range(1, len(word2) + 1) : distance_memory[0][w2] = w2
+
+        for w1 in range(1, len(word1)+1):
+            for w2 in range(1, len(word2) + 1):
+                if word1[w1-1] == word2[w2-1]:
+                    distance_memory[w1][w2] = distance_memory[w1-1][w2-1]
+                else:
+                    distance_memory[w1][w2] = min(distance_memory[w1-1][w2], distance_memory[w1][w2-1]) + 1
+
+        return distance_memory[len(word1)][len(word2)]
 
 if __name__ == "__main__":
     result = Solution().min_distance(word1="leetcode", word2="etco")
