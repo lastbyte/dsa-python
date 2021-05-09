949199eb30533bd05e0ce1cb20d7af96e83733a0
Nishant Kumar <nishant.9c9@gmail.com>
2021-05-09 08:28:20 +0530

    Update pow.py

--- problems/medium/pow.py
+++ problems/medium/pow.py
@@ -11,7 +11,7 @@ def power1(x: float , n: int) -> float:
         return 1
 
     if n%2 == 0:
-        return power1(x, int(n/2)) * power(x, int(n/2))
+        return power1(x, int(n/2)) * power1(x, int(n/2))
     else:
         if n > 0:
             return x * power1(x, int(n/2)) * power1(x, int(n/2))
