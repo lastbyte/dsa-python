ac1e8838ba601efc4a105e4336e57f62480abe2e
nishant <nishant.9c9@gmail.com>
2021-06-16 04:39:27 +0530

    sqrt(x)

---
+++ problems/easy/square_root.py
@@ -0,0 +1,45 @@
+'''
+69. Sqrt(x)
+
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+
+
+Constraints:
+
+0 <= x <= 231 - 1
+
+link -> https://leetcode.com/problems/sqrtx/
+'''
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x;
+        
+        ans = 1;
+        for i in range(1, x//2+1):
+            if i*i <= x:
+                ans = i
+            else:
+                break
+        return ans
+
+if __name__ == "__main__":
+    solution = Solution()
+    result  = solution.mySqrt1(4)
+    print(result)
